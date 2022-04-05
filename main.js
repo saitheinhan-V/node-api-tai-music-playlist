1fbf01c956edd3cebe85fc0f2202a335af443683
saitheinhan-V <saihlainhan1541997@gmail.com>
2022-04-05 19:54:20 +0800

    change routes

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 5001;
+const path = require('path')
 
 const home_route = require('./routes/home_route');
 const chart_route = require('./routes/chart_route');
@@ -10,22 +11,29 @@ const more_route = require('./routes/more_route');
 // app.use('/api',home_route);
 // app.use('/api',chart_route);
 // app.use('/api',more_route);
-app.use('/test',(req, res) => {
+app
+.use('/test',(req, res) => {
   res.json({
     message: "TEST successful"
   })
 })
+.use(express.static(path.join(__dirname, 'public')))
+.set('views', path.join(__dirname, 'views'))
+.set('view engine', 'ejs')
+.get('/', (req, res) => res.render('pages/index'))
+
+
 
 // This should be the last route else any after it won't work
-app.use("/", (req, res) => {
-    res.status(404).json({
-      message: "Page not found",
-      error: {
-        statusCode: 404,
-        message: "You reached a route that is not defined on this server",
-      },
-    });
-  });
+// app.use("/", (req, res) => {
+//     res.status(404).json({
+//       message: "Page not found",
+//       error: {
+//         statusCode: 404,
+//         message: "You reached a route that is not defined on this server",
+//       },
+//     });
+//   });
 
 app.listen(port,()=>{
     console.log('Server is listening at ',port);
