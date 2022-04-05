b8a9da57a7965fe536cd82a6c8ea32c0384db6a7
saitheinhan-V <saihlainhan1541997@gmail.com>
2022-04-05 20:17:31 +0800

    change port 3001

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 
 // const config = process.env;
 dotenv.config();
-const port = process.env.PORT || 3001;
+const port = 3001;
 const path = require('path')
 
 const home_route = require('./routes/home_route');
